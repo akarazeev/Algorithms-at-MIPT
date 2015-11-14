50e4c9baad5e9d86d55c853d3b5a0144dee8f96f
Akarazeev <thawetret@gmail.com>
2015-11-14 11:51:45 +0300

    first commir

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalTasks_1
+//
+//  Created by Anton Wetret on 14/11/15.
+//  Copyright © 2015 Anton Karazeev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
